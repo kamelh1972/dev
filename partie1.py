43bf08d9a2010f938b3bffd4ff357470c5c7d7c3
hammiche <kamelhammiche44@gmail.com>
2019-10-15 23:54:37 +0200

    add exo2

--- partie1.py
+++ partie1.py
@@ -3,3 +3,15 @@ print ("hello-world")
 variable = "hello-world"
 print(variable)
 
+## ex 2
+a = 3
+b = 4
+c = 9
+d = 12
+e = 78
+f = 7
+g = 5
+h = 0
+print ("a*a","=",a*a)
+print ("b+c+e","=",b+c+e)
+print ("d-f","=",d-f)
