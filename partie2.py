9e8f96644da786e9472502384d1f94b8546b97da
hammiche <kamelhammiche44@gmail.com>
2019-10-16 22:45:30 +0200

    add exo3

--- partie2.py
+++ partie2.py
@@ -3,3 +3,14 @@ année_actuelle = int(input("entrer année_actuelle"))
 naissance = int(input("entrer naissance"))
 age = année_actuelle - naissance
 print(age)
+
+##exo3
+prix_chaussure = 70
+prix_jean = 59
+prix_t_shirt = 20
+reduction = 20.0
+prix1 = float(prix_chaussure - reduction)
+prix2 = float(prix_jean - reduction)
+prix3 = float(prix_t_shirt - reduction)
+total = float(prix1 + prix2 + prix3)
+print(total)
